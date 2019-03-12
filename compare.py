60284d74ffa50235300727a2a29e813f1c86f916
Michael Xu <michaeljxu11@gmail.com>
2019-03-12 00:01:28 -0700

    use A/D/M notation like git diff

--- compare.py
+++ compare.py
@@ -13,7 +13,7 @@ def compare_recur(co1, co2, is_dir=False):
     if co1['hash'] == co2['hash'] and co1['namehash'] == co2['namehash']:
         # No need to check!
         return
-    print('{}{}'.format(co1['dirname'], '/' if is_dir else ''))
+    print('M    {}{}'.format(co1['dirname'], '/' if is_dir else ''))
     print_verbose('= {} differs =\n1:{}-{}\n2:{}-{}'.format(
         co1['dirname'],
         co1['hash'],
@@ -30,14 +30,14 @@ def compare_recur(co1, co2, is_dir=False):
         co2_sf_dict = {}
         for sf2 in co2.get(keyname, []):
             co2_sf_dict[sf2['dirname']] = sf2
-        for common_dirname in co1_sf_dict.keys() & co2_sf_dict.keys():
+        for common_dirname in sorted(co1_sf_dict.keys() & co2_sf_dict.keys()):
             compare_recur(co1_sf_dict[common_dirname], co2_sf_dict[common_dirname], is_dir=keyname == 'subdirs')
-        for co1_dir in co1_sf_dict.keys() - co2_sf_dict.keys():
-            print('>>>! {}{}'.format(co1_dir, '/' if keyname == 'subdirs' else ''))
+        for co1_dir in sorted(co1_sf_dict.keys() - co2_sf_dict.keys()):
+            print('A    {}{}'.format(co1_dir, '/' if keyname == 'subdirs' else ''))
             print_verbose('= {} differs ='.format(co1_dir))
             print_verbose('{}: {} in 1 but not 2'.format(keyname, co1_dir))
-        for co2_dir in co2_sf_dict.keys() - co1_sf_dict.keys():
-            print('!<<< {}{}'.format(co2_dir, '/' if keyname == 'subdirs' else ''))
+        for co2_dir in sorted(co2_sf_dict.keys() - co1_sf_dict.keys()):
+            print('D    {}{}'.format(co2_dir, '/' if keyname == 'subdirs' else ''))
             print_verbose('= {} differs ='.format(co2_dir))
             print_verbose('{}: {} in 2 but not 1'.format(keyname, co2_dir))
 
